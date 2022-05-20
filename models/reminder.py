36f86744539dcd3b0e4cc571a08fea6ffdd4301b
Armel <armel@Armels-Mac-mini.local>
2022-05-20 15:16:35 -0400

    completing reminder model

--- models/reminder.py
+++ models/reminder.py
@@ -1,9 +1,12 @@
 
 class Reminder:
-    def __init__(self,person,frequency,startDate):
-        pass
 
-
-
-
-   
+    def __init__(self,personName,reminderNumber,frequency,startDate,onFreeze=False,frozenPeriod= None):
+        self.personName = personName;
+        self.reminderNumber =reminderNumber;
+        self.frequency = frequency;
+        self.startDate=startDate;
+        self.onFreeze = onFreeze;
+        self.frozenPeriod = frozenPeriod;
+    def __repr__(self) -> str:
+        return f"(PersonName:{self.personName},\nreminderNumber:{self.reminderNumber},\nFrequency:{self.frequency},\nStartDate:{self.startDate},\nOnFreeze:{self.onFreeze},\nFrozenPeriod:{self.frozenPeriod})"
